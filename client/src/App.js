10ae46fab5aa0f0666c0b9081080b0aed010f33e
Logan Pritchett <logan.pritchett01@gmail.com>
2024-03-22 08:54:10 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([]);
 
   useEffect(() => {
-    fetch("https://starter-name-generator-client-iagq.onrender.com/generate")
+    fetch("https://starter-name-generator-lfjf.onrender.com/generate")
       .then((res) => res.json())
       .then((nme) => setName([nme]));
   }, []);
